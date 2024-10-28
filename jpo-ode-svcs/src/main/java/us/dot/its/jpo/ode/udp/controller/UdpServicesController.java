8dafd5eb1ebb3ae084dafb2ced3389d327cc9efc
Matt Cook <matt.cook@trihydro.com>
2024-10-28 15:30:17 -0600

    remove unnecessary comments from UdpServicesController

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/udp/controller/UdpServicesController.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/udp/controller/UdpServicesController.java
@@ -25,33 +25,16 @@ public class UdpServicesController {
     public UdpServicesController(UDPReceiverProperties udpProps, OdeKafkaProperties odeKafkaProperties, RawEncodedJsonTopics rawEncodedJsonTopics) {
         super();
 
-        // Start the UDP receivers
         ServiceManager rm = new ServiceManager(new UdpServiceThreadFactory("UdpReceiverManager"));
-
         log.debug("Starting UDP receiver services...");
 
-        // BSM internal
         rm.submit(new BsmReceiver(udpProps.getBsm(), odeKafkaProperties, rawEncodedJsonTopics.getBsm()));
-
-        // TIM internal
         rm.submit(new TimReceiver(udpProps.getTim(), odeKafkaProperties, rawEncodedJsonTopics.getTim()));
-
-        // SSM internal port
         rm.submit(new SsmReceiver(udpProps.getSsm(), odeKafkaProperties, rawEncodedJsonTopics.getSsm()));
-
-        // SRM internal port
         rm.submit(new SrmReceiver(udpProps.getSrm(), odeKafkaProperties, rawEncodedJsonTopics.getSrm()));
-
-        // SPAT internal port
         rm.submit(new SpatReceiver(udpProps.getSpat(), odeKafkaProperties, rawEncodedJsonTopics.getSpat()));
-
-        // MAP internal port
         rm.submit(new MapReceiver(udpProps.getMap(), odeKafkaProperties, rawEncodedJsonTopics.getMap()));
-
-        // PSM internal port
         rm.submit(new PsmReceiver(udpProps.getPsm(), odeKafkaProperties, rawEncodedJsonTopics.getPsm()));
-
-        // Generic Receiver internal port
         rm.submit(new GenericReceiver(udpProps.getGeneric(), odeKafkaProperties, rawEncodedJsonTopics));
 
         log.debug("UDP receiver services started.");
