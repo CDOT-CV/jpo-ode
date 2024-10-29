74ff8b6357547b5c1469143b3c8e8b7cfc450af7
Matt Cook <matt.cook@trihydro.com>
2024-10-29 08:34:22 -0600

    remove redundant @Value annotations from OdeKafkaProperties

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/kafka/OdeKafkaProperties.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/kafka/OdeKafkaProperties.java
@@ -1,7 +1,6 @@
 package us.dot.its.jpo.ode.kafka;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +10,7 @@ import java.util.Set;
 @ConfigurationProperties(prefix = "ode.kafka")
 @Data
 public class OdeKafkaProperties {
-    @Value("${ode.kafka.brokers:localhost:9092}")
     private String brokers;
-    @Value("${ode.kafka.producer-type:sync}")
     private String producerType;
-    @Value("${ode.kafka.disabled-topics:}")
     private Set<String> disabledTopics;
 }
