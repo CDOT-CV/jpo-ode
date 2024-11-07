83bc9dd3c4a66c7b3327b82f8c4db9df337330cd
Matt Cook <matt.cook@trihydro.com>
2024-11-07 11:19:57 -0700

    Use embedded kafka to confirm UDP map receiver sends message to topic.OdeRawEncodedMAPJson via kafka

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/udp/map/MapReceiverIntegrationTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/udp/map/MapReceiverIntegrationTest.java
@@ -1,20 +1,25 @@
 package us.dot.its.jpo.ode.udp.map;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.junit.ClassRule;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
+import org.springframework.test.context.junit4.SpringRunner;
 import us.dot.its.jpo.ode.kafka.OdeKafkaProperties;
 import us.dot.its.jpo.ode.kafka.RawEncodedJsonTopics;
 import us.dot.its.jpo.ode.udp.TestUDPClient;
@@ -26,14 +31,18 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(initializers = ConfigDataApplicationContextInitializer.class)
 @EnableConfigurationProperties(value = {UDPReceiverProperties.class, OdeKafkaProperties.class, RawEncodedJsonTopics.class, KafkaProperties.class})
-@Testcontainers
-@TestPropertySource(properties = {"spring.kafka.consumer.auto-offset-reset=earliest"})
+@RunWith(SpringRunner.class)
+@DirtiesContext
+@Slf4j
 class MapReceiverIntegrationTest {
 
     @Autowired
@@ -45,10 +54,11 @@ class MapReceiverIntegrationTest {
     @Autowired
     RawEncodedJsonTopics rawEncodedJsonTopics;
 
-    @Container
-    static final KafkaContainer kafka = new KafkaContainer(
-            DockerImageName.parse("confluentinc/cp-kafka:7.6.1")
-    );
+    @ClassRule
+    private static EmbeddedKafkaRule embeddedKafka = new EmbeddedKafkaRule(1,
+            false,
+            1,
+            "topic.OdeRawEncodedMAPJson").kafkaPorts(9092);
 
     ServiceManager rm;
     TestUDPClient udpClient;
@@ -66,6 +76,7 @@ class MapReceiverIntegrationTest {
     @BeforeEach
     public void setUp() {
         rm = new ServiceManager(new UdpServiceThreadFactory("UdpReceiverManager"));
+        embeddedKafka.before();
         mapReceiver = new MapReceiver(udpReceiverProperties.getMap(),
                 odeKafkaProperties,
                 rawEncodedJsonTopics.getMap());
@@ -78,7 +89,7 @@ class MapReceiverIntegrationTest {
         udpClient.close();
     }
 
-    class TestRow {
+    static class TestRow {
         String messageType;
         String timestamp;
         String payload;
@@ -114,7 +125,14 @@ class MapReceiverIntegrationTest {
         udpClient = new TestUDPClient(udpReceiverProperties.getMap().getReceiverPort());
 
         udpClient.send(rows.getFirst().payload);
-        // TODO: confirm that the OdeRawEncodedMapJson topic has the expected message
+
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("testT", "false", embeddedKafka.getEmbeddedKafka());
+        DefaultKafkaConsumerFactory<Integer, String> cf = new DefaultKafkaConsumerFactory<>(consumerProps);
+        Consumer<Integer, String> consumer = cf.createConsumer();
+        embeddedKafka.getEmbeddedKafka().consumeFromAllEmbeddedTopics(consumer);
+
+        ConsumerRecords<Integer, String> replies = KafkaTestUtils.getRecords(consumer);
+        assertTrue(replies.count() > 0);
 
         // Wait for the MapReceiver to process the packet
         // Verify that the MapReceiver produced the expected output on the expected topic
