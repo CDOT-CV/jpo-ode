5a7e719421976e29f79be3f1487ad3c3193b759f
Matt Cook <matt.cook@trihydro.com>
2024-11-19 15:46:56 -0700

    inject test queue names with SpringBootTest annotation in MapReceiverTest to support unique test topic naming and port assignment

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/udp/map/MapReceiverTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/udp/map/MapReceiverTest.java
@@ -7,12 +7,12 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
-import us.dot.its.jpo.ode.kafka.topics.RawEncodedJsonTopics;
 import us.dot.its.jpo.ode.model.OdeMsgMetadata;
 import us.dot.its.jpo.ode.testUtilities.ApprovalTestCase;
 import us.dot.its.jpo.ode.testUtilities.EmbeddedKafkaHolder;
@@ -31,7 +31,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static us.dot.its.jpo.ode.testUtilities.ApprovalTestCase.deserializeTestCases;
 
 @Slf4j
-@SpringBootTest
+@SpringBootTest(properties = {"ode.kafka.topics.raw-encoded-json.map=topic.MapReceiverTestMAPJSON", "ode.receivers.map.receiver-port=12412"})
 @DirtiesContext
 class MapReceiverTest {
     private final EmbeddedKafkaBroker embeddedKafka = EmbeddedKafkaHolder.getEmbeddedKafka();
@@ -39,8 +39,11 @@ class MapReceiverTest {
     @Autowired
     UDPReceiverProperties udpReceiverProperties;
 
-    @Autowired
-    RawEncodedJsonTopics rawEncodedJsonTopics;
+    @Value(value = "${ode.kafka.topics.raw-encoded-json.map}")
+    private String rawJsonMapTopic;
+
+    @Value(value = "${ode.receivers.map.receiver-port}")
+    private int mapReceiverPort;
 
     @Test
     void testMapReceiver() throws IOException {
@@ -56,27 +59,27 @@ class MapReceiverTest {
         OdeMsgMetadata.setStaticSchemaVersion(7);
 
         try {
-            embeddedKafka.addTopics(new NewTopic(rawEncodedJsonTopics.getMap(), 1, (short) 1));
+            embeddedKafka.addTopics(new NewTopic(rawJsonMapTopic, 1, (short) 1));
         } catch (Exception e) {
-            log.warn("Couldn't create topics. If the error indicates the topics already exist, this message is safe to ignore", e);
+            log.warn("Couldn't create topics. If the error indicates the topics already exist, this message is safe to ignore: {}", e.getMessage());
         }
 
         // Set up a Kafka consumer
         Map<String, Object> consumerProps = KafkaTestUtils.consumerProps("test-group", "false", embeddedKafka);
         DefaultKafkaConsumerFactory<Integer, String> cf = new DefaultKafkaConsumerFactory<>(consumerProps);
         Consumer<Integer, String> consumer = cf.createConsumer();
-        embeddedKafka.consumeFromAnEmbeddedTopic(consumer, rawEncodedJsonTopics.getMap());
+        embeddedKafka.consumeFromAnEmbeddedTopic(consumer, rawJsonMapTopic);
 
         // Clarifying note (for Spring newbies):
         // UdpServicesController is annotated with @Controller, and this test class is annotated with @SpringBootTest
         // so a UdpServicesController will be instantiated before this test runs. That means the MapReceiver will also
         // be instantiated and ready to consume UDP traffic from the same port we configure the TestUDPClient to send packets to.
-        TestUDPClient udpClient = new TestUDPClient(udpReceiverProperties.getMap().getReceiverPort());
+        TestUDPClient udpClient = new TestUDPClient(mapReceiverPort);
 
         for (ApprovalTestCase approvalTestCase : approvalTestCases) {
             udpClient.send(approvalTestCase.getInput());
 
-            ConsumerRecord<Integer, String> produced = KafkaTestUtils.getSingleRecord(consumer, rawEncodedJsonTopics.getMap());
+            ConsumerRecord<Integer, String> produced = KafkaTestUtils.getSingleRecord(consumer, rawJsonMapTopic);
 
             JSONObject producedJson = new JSONObject(produced.value());
             JSONObject expectedJson = new JSONObject(approvalTestCase.getExpected());
