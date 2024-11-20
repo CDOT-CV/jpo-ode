66c5b64ead99fba5a1167163137739282d54ff17
Matt Cook <matt.cook@trihydro.com>
2024-11-19 17:05:56 -0700

    inject topic names into Asn1DecodedDataRouterApprovalTest to keep topics under test separate between tests

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/services/asn1/Asn1DecodedDataRouterApprovalTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/services/asn1/Asn1DecodedDataRouterApprovalTest.java
@@ -6,7 +6,9 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -24,21 +26,30 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
-@SpringBootTest
+@SpringBootTest(properties = {
+        "ode.kafka.topics.asn1.decoder-output=topic.Asn1DecoderOutputRouterApprovalTest",
+        "ode.kafka.topics.pojo.tx-map=topic.OdeMapTxPojoRouterApprovalTest",
+        "ode.kafka.topics.json.map=topic.OdeMapJsonRouterApprovalTest"
+})
 @DirtiesContext
 class Asn1DecodedDataRouterApprovalTest {
 
-    static final String INPUT_TOPIC = "topic.Asn1DecoderOutput";
-    static final String OUTPUT_TOPIC_TX = "topic.OdeMapTxPojo";
-    static final String OUTPUT_TOPIC_JSON = "topic.OdeMapJson";
+    @Value("${ode.kafka.topics.asn1.decoder-output}")
+    private String decoderOutputTopic;
+
+    @Value("${ode.kafka.topics.pojo.tx-map}")
+    private String txMapTopic;
+
+    @Value("${ode.kafka.topics.json.map}")
+    private String jsonMapTopic;
 
     EmbeddedKafkaBroker embeddedKafka = EmbeddedKafkaHolder.getEmbeddedKafka();
 
     @Test
     void testAsn1DecodedDataRouter() throws IOException {
-        NewTopic inputTopic = new NewTopic(INPUT_TOPIC, 1, (short) 1);
-        NewTopic outputTopicTx = new NewTopic(OUTPUT_TOPIC_TX, 1, (short) 1);
-        NewTopic outputTopicJson = new NewTopic(OUTPUT_TOPIC_JSON, 1, (short) 1);
+        NewTopic inputTopic = new NewTopic(decoderOutputTopic, 1, (short) 1);
+        NewTopic outputTopicTx = new NewTopic(txMapTopic, 1, (short) 1);
+        NewTopic outputTopicJson = new NewTopic(jsonMapTopic, 1, (short) 1);
         try {
             embeddedKafka.addTopics(inputTopic, outputTopicTx, outputTopicJson);
         } catch (RuntimeException e) {
@@ -57,28 +68,29 @@ class Asn1DecodedDataRouterApprovalTest {
         DefaultKafkaConsumerFactory<Integer, String> cf = new DefaultKafkaConsumerFactory<>(consumerProps);
 
         Consumer<Integer, String> consumer = cf.createConsumer();
-        embeddedKafka.consumeFromEmbeddedTopics(consumer, OUTPUT_TOPIC_TX, OUTPUT_TOPIC_JSON);
+        embeddedKafka.consumeFromEmbeddedTopics(consumer, txMapTopic, jsonMapTopic);
 
         for (ApprovalTestCase testCase : testCases) {
-            ProducerRecord<Integer, String> producerRecord = new ProducerRecord<>(INPUT_TOPIC, 0, 0, testCase.getInput());
-            producer.send(producerRecord);
+            ProducerRecord<Integer, String> producerRecord = new ProducerRecord<>(decoderOutputTopic, 0, 0, testCase.getInput());
+            var sent = producer.send(producerRecord);
+            Awaitility.await().until(sent::isDone);
 
-            String received = KafkaTestUtils.getSingleRecord(consumer, OUTPUT_TOPIC_TX).value();
+            String received = KafkaTestUtils.getSingleRecord(consumer, txMapTopic).value();
             ObjectMapper mapper = new ObjectMapper();
             OdeMapData receivedMapData = mapper.readValue(received, OdeMapData.class);
             OdeMapData expectedMapData = mapper.readValue(testCase.getExpected(), OdeMapData.class);
             assertEquals(expectedMapData.toJson(), receivedMapData.toJson(), "Failed test case: " + testCase.getDescription());
             // discard the JSON output
-            KafkaTestUtils.getSingleRecord(consumer, OUTPUT_TOPIC_JSON);
+            KafkaTestUtils.getSingleRecord(consumer, jsonMapTopic);
         }
 
         List<ApprovalTestCase> jsonTestCases = ApprovalTestCase.deserializeTestCases("src/test/resources/us.dot.its.jpo.ode.udp.map/Asn1DecoderRouter_ApprovalTestCases_MapJson.json");
 
         for (ApprovalTestCase testCase : jsonTestCases) {
-            ProducerRecord<Integer, String> producerRecord = new ProducerRecord<>(INPUT_TOPIC, 0, 0, testCase.getInput());
+            ProducerRecord<Integer, String> producerRecord = new ProducerRecord<>(decoderOutputTopic, 0, 0, testCase.getInput());
             producer.send(producerRecord);
 
-            String received = KafkaTestUtils.getSingleRecord(consumer, OUTPUT_TOPIC_JSON).value();
+            String received = KafkaTestUtils.getSingleRecord(consumer, jsonMapTopic).value();
             ObjectMapper mapper = new ObjectMapper();
             OdeMapData receivedMapData = mapper.readValue(received, OdeMapData.class);
             OdeMapData expectedMapData = mapper.readValue(testCase.getExpected(), OdeMapData.class);
