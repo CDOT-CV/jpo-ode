038e24e42db632c77fb9ae3075a934d8ca2dafaa
Matt Cook <matt.cook@trihydro.com>
2024-12-02 16:05:44 -0700

    reformat TimReceiver

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/udp/tim/TimReceiver.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/udp/tim/TimReceiver.java
@@ -12,43 +12,43 @@ import us.dot.its.jpo.ode.udp.controller.UDPReceiverProperties.ReceiverPropertie
 @Slf4j
 public class TimReceiver extends AbstractUdpReceiverPublisher {
 
-    private final KafkaTemplate<String, String> timPublisher;
-    private final String publishTopic;
+  private final KafkaTemplate<String, String> timPublisher;
+  private final String publishTopic;
 
-    public TimReceiver(ReceiverProperties receiverProperties,
-        KafkaTemplate<String, String> kafkaTemplate, String publishTopic) {
-        super(receiverProperties.getReceiverPort(), receiverProperties.getBufferSize());
+  public TimReceiver(ReceiverProperties receiverProperties,
+      KafkaTemplate<String, String> kafkaTemplate, String publishTopic) {
+    super(receiverProperties.getReceiverPort(), receiverProperties.getBufferSize());
 
-        this.publishTopic = publishTopic;
-        this.timPublisher = kafkaTemplate;
-    }
+    this.publishTopic = publishTopic;
+    this.timPublisher = kafkaTemplate;
+  }
 
-    @Override
-    public void run() {
-        log.debug("TIM UDP Receiver Service started.");
+  @Override
+  public void run() {
+    log.debug("TIM UDP Receiver Service started.");
 
-        byte[] buffer = new byte[bufferSize];
+    byte[] buffer = new byte[bufferSize];
 
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
-        do {
-            try {
-                log.debug("Waiting for UDP TIM packets...");
-                socket.receive(packet);
-                if (packet.getLength() > 0) {
+    do {
+      try {
+        log.debug("Waiting for UDP TIM packets...");
+        socket.receive(packet);
+        if (packet.getLength() > 0) {
 
-                    String timJson = UdpHexDecoder.buildJsonTimFromPacket(packet);
-                    if (timJson != null) {
-                        timPublisher.send(publishTopic, timJson);
-                    }
-                }
-            } catch (InvalidPayloadException e) {
-                log.error("Error decoding packet", e);
-            } catch (Exception e) {
-                log.error("Error receiving packet", e);
-            }
-        } while (!isStopped());
-    }
+          String timJson = UdpHexDecoder.buildJsonTimFromPacket(packet);
+          if (timJson != null) {
+            timPublisher.send(publishTopic, timJson);
+          }
+        }
+      } catch (InvalidPayloadException e) {
+        log.error("Error decoding packet", e);
+      } catch (Exception e) {
+        log.error("Error receiving packet", e);
+      }
+    } while (!isStopped());
+  }
 
 
 }
