ed7e0136f86faa080b3f19f8bd19aed5b1f6ebd0
Matt Cook <matt.cook@trihydro.com>
2024-12-03 17:19:17 -0700

    corrected various checkstyle issues in SpatReceiver

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/udp/spat/SpatReceiver.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/udp/spat/SpatReceiver.java
@@ -8,41 +8,59 @@ import us.dot.its.jpo.ode.udp.InvalidPayloadException;
 import us.dot.its.jpo.ode.udp.UdpHexDecoder;
 import us.dot.its.jpo.ode.udp.controller.UDPReceiverProperties.ReceiverProperties;
 
+/**
+ * The SpatReceiver class is responsible for receiving UDP packets containing SPaT (Signal Phase and Timing) data,
+ * decoding these packets, and publishing the resulting JSON string to a specified Kafka topic.
+ *
+ * </p>
+ * This class extends the AbstractUdpReceiverPublisher, allowing it to run as a separate thread and handle incoming UDP packets.
+ * It utilizes a Kafka template to publish decoded data.
+ */
 @Slf4j
 public class SpatReceiver extends AbstractUdpReceiverPublisher {
 
-    private final KafkaTemplate<String, String> spatPublisher;
-    private final String publishTopic;
+  private final KafkaTemplate<String, String> spatPublisher;
+  private final String publishTopic;
 
-    public SpatReceiver(
-        ReceiverProperties receiverProperties, KafkaTemplate<String, String> kafkaTemplate, String publishTopic) {
-        super(receiverProperties.getReceiverPort(), receiverProperties.getBufferSize());
+  /**
+   * Constructs a SpatReceiver object that is responsible for receiving UDP packets containing SPaT
+   * (Signal Phase and Timing) data, decoding these packets, and publishing the resulting JSON string
+   * to a specified Kafka topic.
+   *
+   * @param receiverProperties the properties for the receiver including the port and buffer size
+   * @param kafkaTemplate the Kafka template used for publishing the decoded SPaT data
+   * @param publishTopic the Kafka topic to which the decoded SPaT data will be published
+   */
+  public SpatReceiver(
+      ReceiverProperties receiverProperties, KafkaTemplate<String, String> kafkaTemplate,
+      String publishTopic) {
+    super(receiverProperties.getReceiverPort(), receiverProperties.getBufferSize());
 
-        this.publishTopic = publishTopic;
-        this.spatPublisher = kafkaTemplate;
-    }
+    this.publishTopic = publishTopic;
+    this.spatPublisher = kafkaTemplate;
+  }
 
-    @Override
-    public void run() {
-        log.debug("SPaT UDP Receiver Service started.");
+  @Override
+  public void run() {
+    log.debug("SPaT UDP Receiver Service started.");
 
-        byte[] buffer = new byte[bufferSize];
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-        do {
-            try {
-                log.debug("Waiting for UDP SPaT packets...");
-                socket.receive(packet);
-                if (packet.getLength() > 0) {
-                    String spatJson = UdpHexDecoder.buildJsonSpatFromPacket(packet);
-                    if (spatJson != null) {
-                        spatPublisher.send(publishTopic, spatJson);
-                    }
-                }
-            } catch (InvalidPayloadException e) {
-                log.error("Error decoding packet", e);
-            } catch (Exception e) {
-                log.error("Error receiving packet", e);
-            }
-        } while (!isStopped());
-    }
+    byte[] buffer = new byte[bufferSize];
+    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+    do {
+      try {
+        log.debug("Waiting for UDP SPaT packets...");
+        socket.receive(packet);
+        if (packet.getLength() > 0) {
+          String spatJson = UdpHexDecoder.buildJsonSpatFromPacket(packet);
+          if (spatJson != null) {
+            spatPublisher.send(publishTopic, spatJson);
+          }
+        }
+      } catch (InvalidPayloadException e) {
+        log.error("Error decoding packet", e);
+      } catch (Exception e) {
+        log.error("Error receiving packet", e);
+      }
+    } while (!isStopped());
+  }
 }
