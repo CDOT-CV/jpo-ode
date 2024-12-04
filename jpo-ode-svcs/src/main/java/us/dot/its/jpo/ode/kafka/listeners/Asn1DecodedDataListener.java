7719a8632d80ed8fa348cab6ad47ef567d365bdd
Matt Cook <matt.cook@trihydro.com>
2024-12-04 14:21:46 -0700

    reformatted Asn1DecodedDataListener
    Signed-off-by: Matt Cook <matt.cook@trihydro.com>

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/kafka/listeners/Asn1DecodedDataListener.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/kafka/listeners/Asn1DecodedDataListener.java
@@ -1,5 +1,7 @@
 package us.dot.its.jpo.ode.kafka.listeners;
 
+import java.util.Map;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaHandler;
@@ -15,68 +17,71 @@ import us.dot.its.jpo.ode.model.OdeAsn1Data;
 import us.dot.its.jpo.ode.model.OdeLogMetadata;
 import us.dot.its.jpo.ode.util.XmlUtils;
 
-import java.util.Map;
-import java.util.Set;
-
 @Slf4j
 @Component
-@KafkaListener(id = "Asn1DecodedDataListener", topics = "${ode.kafka.topics.asn1.decoder-output}", containerFactory = "tempFilteringKafkaListenerContainerFactory")
+@KafkaListener(
+    id = "Asn1DecodedDataListener",
+    topics = "${ode.kafka.topics.asn1.decoder-output}",
+    containerFactory = "tempFilteringKafkaListenerContainerFactory"
+)
 public class Asn1DecodedDataListener {
 
 
-    private final String jsonMapTopic;
-    private final String pojoTxMapTopic;
-    private final KafkaTemplate<String, String> kafkaTemplate;
-    private final Set<String> disabledTopics;
+  private final String jsonMapTopic;
+  private final String pojoTxMapTopic;
+  private final KafkaTemplate<String, String> kafkaTemplate;
+  private final Set<String> disabledTopics;
 
-    public Asn1DecodedDataListener(KafkaTemplate<String, String> kafkaTemplate,
-                                   @Value("${ode.kafka.topics.pojo.tx-map}") String pojoTxMapTopic,
-                                   @Value("${ode.kafka.topics.json.map}") String jsonMapTopic,
-                                   OdeKafkaProperties odeKafkaProperties) {
-        this.kafkaTemplate = kafkaTemplate;
-        this.pojoTxMapTopic = pojoTxMapTopic;
-        this.jsonMapTopic = jsonMapTopic;
-        this.disabledTopics = odeKafkaProperties.getDisabledTopics();
+  public Asn1DecodedDataListener(KafkaTemplate<String, String> kafkaTemplate,
+      @Value("${ode.kafka.topics.pojo.tx-map}") String pojoTxMapTopic,
+      @Value("${ode.kafka.topics.json.map}") String jsonMapTopic,
+      OdeKafkaProperties odeKafkaProperties) {
+    this.kafkaTemplate = kafkaTemplate;
+    this.pojoTxMapTopic = pojoTxMapTopic;
+    this.jsonMapTopic = jsonMapTopic;
+    this.disabledTopics = odeKafkaProperties.getDisabledTopics();
+  }
+
+  @KafkaHandler
+  public void listenToMAPs(@Headers Map<String, Object> keys, @Payload String payload) {
+    log.debug("Key: {} payload: {}", keys, payload);
+    try {
+      String odeMapData = OdeMapDataCreatorHelper.createOdeMapData(payload).toString();
+
+      OdeLogMetadata.RecordType recordType = OdeLogMetadata.RecordType
+          .valueOf(XmlUtils.toJSONObject(payload)
+              .getJSONObject(OdeAsn1Data.class.getSimpleName())
+              .getJSONObject(AppContext.METADATA_STRING)
+              .getString("recordType")
+          );
+      if (recordType == OdeLogMetadata.RecordType.mapTx) {
+        log.debug("Publishing message with recordType: {} to {} ", recordType,
+            pojoTxMapTopic);
+        send(odeMapData, pojoTxMapTopic);
+      }
+
+      // Send all MAP data to OdeMapJson despite the record type
+      send(odeMapData, jsonMapTopic);
+    } catch (Exception e) {
+      log.error(e.getMessage(), e);
+    }
+  }
+
+  private void send(String odeMapData, String publishTopic) {
+    if (disabledTopics.contains(publishTopic)) {
+      log.debug("Topic {} is disabled. Skipping sending message.", publishTopic);
+      return;
     }
 
-    @KafkaHandler
-    public void listenToMAPs(@Headers Map<String, Object> keys, @Payload String payload) {
-        log.debug("Key: {} payload: {}", keys, payload);
-        try {
-            String odeMapData = OdeMapDataCreatorHelper.createOdeMapData(payload).toString();
-
-            OdeLogMetadata.RecordType recordType = OdeLogMetadata.RecordType
-                    .valueOf(XmlUtils.toJSONObject(payload)
-                            .getJSONObject(OdeAsn1Data.class.getSimpleName())
-                            .getJSONObject(AppContext.METADATA_STRING)
-                            .getString("recordType")
-                    );
-            if (recordType == OdeLogMetadata.RecordType.mapTx) {
-                log.debug("Publishing message with recordType: {} to {} ", recordType, pojoTxMapTopic);
-                send(odeMapData, pojoTxMapTopic);
-            }
-
-            // Send all MAP data to OdeMapJson despite the record type
-            send(odeMapData, jsonMapTopic);
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-        }
-    }
-
-    private void send(String odeMapData, String publishTopic) {
-        if (disabledTopics.contains(publishTopic)) {
-            log.debug("Topic {} is disabled. Skipping sending message.", publishTopic);
-            return;
-        }
-
-        var future = kafkaTemplate.send(publishTopic, odeMapData);
-        future.whenComplete((result, ex) -> {
-            if (ex != null) {
-                log.error(ex.getMessage(), ex);
-            } else {
-                log.debug("Successfully sent message to topic {} with offset {} on partition {}",
-                        publishTopic, result.getRecordMetadata().offset(), result.getRecordMetadata().partition());
-            }
-        });
-    }
+    var future = kafkaTemplate.send(publishTopic, odeMapData);
+    future.whenComplete((result, ex) -> {
+      if (ex != null) {
+        log.error(ex.getMessage(), ex);
+      } else {
+        log.debug("Successfully sent message to topic {} with offset {} on partition {}",
+            publishTopic, result.getRecordMetadata().offset(),
+            result.getRecordMetadata().partition());
+      }
+    });
+  }
 }
