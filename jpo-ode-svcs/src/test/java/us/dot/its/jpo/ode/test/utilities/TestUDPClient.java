5a8f903fd3b68f5370e04da561a5ae40dc1945e5
Matt Cook <matt.cook@trihydro.com>
2024-12-04 09:11:26 -0700

    corrected various checkstyle issues in TestUDPClient.java
    Signed-off-by: Matt Cook <matt.cook@trihydro.com>

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/test/utilities/TestUDPClient.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/test/utilities/TestUDPClient.java
@@ -10,25 +10,23 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.buf.HexUtils;
 
 /**
- * TestUDPClient is a utility class used for sending UDP packets to a specified port.
- * It encapsulates the creation of a DatagramSocket and the logic to send data
- * packets to a local server.
+ * TestUDPClient is a utility class used for sending UDP packets to a specified port. It
+ * encapsulates the creation of a DatagramSocket and the logic to send data packets to a local
+ * server.
  */
 @Slf4j
 public class TestUDPClient {
 
-  private DatagramSocket socket;
-  private InetAddress address;
-  private int sendPort;
-
-  private byte[] buf;
+  private final DatagramSocket socket;
+  private final InetAddress address;
+  private final int sendPort;
 
   /**
-   * Constructs a new TestUDPClient instance configured to send UDP packets
-   * to the specified port on the localhost.
+   * Constructs a new TestUDPClient instance configured to send UDP packets to the specified port on
+   * the localhost.
    *
    * @param port the port number on the localhost to which UDP packets will be sent
-   * @throws SocketException if there is an error creating the DatagramSocket
+   * @throws SocketException      if there is an error creating the DatagramSocket
    * @throws UnknownHostException if the local host cannot be resolved
    */
   public TestUDPClient(int port) throws SocketException, UnknownHostException {
@@ -38,15 +36,14 @@ public class TestUDPClient {
   }
 
   /**
-   * Sends a UDP packet containing the specified message to the configured port
-   * on the localhost. The message is converted from a hexadecimal string to a
-   * byte array before being sent.
+   * Sends a UDP packet containing the specified message to the configured port on the localhost.
+   * The message is converted from a hexadecimal string to a byte array before being sent.
    *
    * @param msg the message to be sent, represented as a hexadecimal string
    * @throws IOException if an I/O error occurs during the sending process
    */
   public void send(String msg) throws IOException {
-    buf = HexUtils.fromHexString(msg);
+    byte[] buf = HexUtils.fromHexString(msg);
 
     DatagramPacket packet = new DatagramPacket(buf, buf.length, address, sendPort);
     socket.send(packet);
