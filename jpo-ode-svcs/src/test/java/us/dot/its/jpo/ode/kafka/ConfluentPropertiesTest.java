7895d5b0f6f9b8557eeed52d91993a73abc6a507
Matt Cook <matt.cook@trihydro.com>
2024-12-04 08:48:26 -0700

    formatted ConfluentPropertiesTest
    Signed-off-by: Matt Cook <matt.cook@trihydro.com>

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/kafka/ConfluentPropertiesTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/kafka/ConfluentPropertiesTest.java
@@ -18,7 +18,9 @@ class ConfluentPropertiesTest {
     expected.put("ssl.endpoint.identification.algorithm", "https");
     expected.put("security.protocol", "SASL_SSL");
     expected.put("sasl.mechanism", "PLAIN");
-    expected.put("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"username322\" password=\"password123\";");
+    expected.put("sasl.jaas.config",
+        "org.apache.kafka.common.security.plain.PlainLoginModule "
+            + "required username=\"username322\" password=\"password123\";");
 
     assertEquals(expected, confluentProperties.buildConfluentProperties());
   }
