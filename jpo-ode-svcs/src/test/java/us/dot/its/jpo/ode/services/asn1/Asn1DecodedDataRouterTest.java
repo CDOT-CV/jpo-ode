691e8a84f5f2e90a560d44c7349f37f7bab5dc34
Matt Cook <matt.cook@trihydro.com>
2024-12-10 11:56:50 -0700

    test: added test for BSMDataFlow to Asn1DecodedDataRouterTest
    Signed-off-by: Matt Cook <matt.cook@trihydro.com>

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/services/asn1/Asn1DecodedDataRouterTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/services/asn1/Asn1DecodedDataRouterTest.java
@@ -1,31 +1,135 @@
 package us.dot.its.jpo.ode.services.asn1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
+import us.dot.its.jpo.ode.kafka.KafkaConsumerConfig;
 import us.dot.its.jpo.ode.kafka.OdeKafkaProperties;
+import us.dot.its.jpo.ode.kafka.producer.KafkaProducerConfig;
+import us.dot.its.jpo.ode.kafka.topics.Asn1CoderTopics;
+import us.dot.its.jpo.ode.kafka.topics.PojoTopics;
 import us.dot.its.jpo.ode.kafka.topics.RawEncodedJsonTopics;
+import us.dot.its.jpo.ode.services.asn1.message.AsnCodecMessageServiceController;
+import us.dot.its.jpo.ode.test.utilities.EmbeddedKafkaHolder;
 import us.dot.its.jpo.ode.udp.controller.UDPReceiverProperties;
 
 @Slf4j
-@SpringBootTest()
+@SpringBootTest(
+    classes = {
+        AsnCodecMessageServiceController.class,
+        KafkaProperties.class,
+        PojoTopics.class,
+        KafkaProducerConfig.class,
+        KafkaConsumerConfig.class,
+        RawEncodedJsonTopics.class,
+        Asn1CoderTopics.class,
+    },
+    properties = {
+        "ode.kafka.topics.pojo.bsm-during-event=topic.BsmDuringEventAsn1DecodedDataRouterTest",
+        "ode.kafka.topics.pojo.rx-bsm=topic.RxBsmAsn1DecodedDataRouterTest",
+        "ode.kafka.topics.pojo.tx-bsm=topic.TxBsmAsn1DecodedDataRouterTest",
+        "ode.kafka.topics.pojo.bsm=topic.BsmAsn1DecodedDataRouterTest",
+    }
+)
 @EnableConfigurationProperties
 @ContextConfiguration(classes = {
     UDPReceiverProperties.class, OdeKafkaProperties.class,
-    RawEncodedJsonTopics.class, KafkaProperties.class
+    PojoTopics.class, KafkaProperties.class
 })
 @DirtiesContext
 class Asn1DecodedDataRouterTest {
 
+  EmbeddedKafkaBroker embeddedKafka = EmbeddedKafkaHolder.getEmbeddedKafka();
+  @Autowired
+  KafkaTemplate<String, String> kafkaStringTemplate;
+  @Autowired
+  PojoTopics pojoTopics;
+  @Qualifier("kafkaListenerContainerFactory")
+  @Autowired
+  ConcurrentKafkaListenerContainerFactory<String, String> listenerContainerFactory;
+
   @Test
   void testAsn1DecodedDataRouter_BSMDataFlow() {
-    fail("Not yet implemented");
+    try {
+      embeddedKafka.addTopics(
+          new NewTopic(pojoTopics.getBsm(), 1, (short) 1),
+          new NewTopic(pojoTopics.getBsmDuringEvent(), 1, (short) 1),
+          new NewTopic(pojoTopics.getRxBsm(), 1, (short) 1),
+          new NewTopic(pojoTopics.getTxBsm(), 1, (short) 1)
+      );
+    } catch (Exception e) {
+      // Ignore because we only care they are created not that they weren't created prior
+    }
+
+    String baseTestData =
+        loadFromResource("us/dot/its/jpo/ode/services/asn1/decoder-output-bsm.xml");
+    String baseExpectedSpecificBsm =
+        loadFromResource("us/dot/its/jpo/ode/services/asn1/expected-bsm-specific.xml");
+    String baseExpectedBsm = loadFromResource("us/dot/its/jpo/ode/services/asn1/expected-bsm.xml");
+
+    var testConsumer =
+        listenerContainerFactory.getConsumerFactory().createConsumer();
+    embeddedKafka.consumeFromAllEmbeddedTopics(testConsumer);
+
+    for (String recordType : new String[] {"bsmLogDuringEvent", "rxMsg", "bsmTx"}) {
+      String topic;
+      switch (recordType) {
+        case "bsmLogDuringEvent" -> topic = pojoTopics.getBsmDuringEvent();
+        case "rxMsg" -> topic = pojoTopics.getRxBsm();
+        case "bsmTx" -> topic = pojoTopics.getTxBsm();
+        default -> throw new IllegalStateException("Unexpected value: " + recordType);
+      }
+
+      String inputData = baseTestData.replace("<recordType>bsmTx</recordType>",
+          "<recordType>" + recordType + "</recordType>");
+      kafkaStringTemplate.send(topic, inputData);
+      kafkaStringTemplate.send(pojoTopics.getBsm(), inputData);
+
+      var consumedSpecific = KafkaTestUtils.getSingleRecord(testConsumer, topic);
+      var consumedBsm = KafkaTestUtils.getSingleRecord(testConsumer, pojoTopics.getBsm());
+
+      String expectedBsm = baseExpectedBsm.replace(
+          "<recordType>bsmLogDuringEvent</recordType>",
+          "<recordType>" + recordType + "</recordType>");
+      assertEquals(expectedBsm, consumedBsm.value());
+
+      String expectedSpecificBsm = baseExpectedSpecificBsm.replace(
+          "<recordType>bsmLogDuringEvent</recordType>",
+          "<recordType>" + recordType + "</recordType>");
+      assertEquals(expectedSpecificBsm, consumedSpecific.value());
+    }
+  }
+
+  private String loadFromResource(String resourcePath) {
+    String baseTestData;
+    try (InputStream inputStream = getClass().getClassLoader()
+        .getResourceAsStream(resourcePath)) {
+      if (inputStream == null) {
+        throw new FileNotFoundException("Resource not found: decoder-output-bsm.xml");
+      }
+      baseTestData = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to load test data from decoder-output-bsm.xml", e);
+    }
+    return baseTestData;
   }
 
   @Test
