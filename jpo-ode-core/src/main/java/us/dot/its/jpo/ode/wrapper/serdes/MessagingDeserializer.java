019190bd6567112a6adacc0031a2dc5de9707177
Matt Cook <matt.cook@trihydro.com>
2024-12-11 15:16:48 -0700

    refactor: seal serializers and deserializers for improved structure.
    Removed unused J2735-specific classes and introduced sealed classes for `MessagingSerializer` and `MessagingDeserializer` to enforce subtyping rules. Marked `OdeBsmSerializer`, `OdeBsmDeserializer`, `OdeTimSerializer`, and `OdeTimDeserializer` as deprecated, promoting a cleaner and more maintainable design.

--- jpo-ode-core/src/main/java/us/dot/its/jpo/ode/wrapper/serdes/MessagingDeserializer.java
+++ jpo-ode-core/src/main/java/us/dot/its/jpo/ode/wrapper/serdes/MessagingDeserializer.java
@@ -15,29 +15,16 @@
  ******************************************************************************/
 package us.dot.its.jpo.ode.wrapper.serdes;
 
-import java.util.Map;
-
 import org.apache.kafka.common.serialization.Deserializer;
-
 import us.dot.its.jpo.ode.util.SerializationUtils;
 
-public class MessagingDeserializer<T> implements Deserializer<T> {
+public sealed class MessagingDeserializer<T> implements Deserializer<T>
+    permits OdeBsmDeserializer {
 
-    SerializationUtils<T> deserializer = new SerializationUtils<T>();
-
-    @Override
-    public void configure(Map<String, ?> configs, boolean isKey) {
-        // nothing to do
-    }
+    SerializationUtils<T> deserializer = new SerializationUtils<>();
 
     @Override
     public T deserialize(String topic, byte[] data) {
         return deserializer.deserialize(data);
     }
-
-    @Override
-    public void close() {
-        // nothing to do
-    }
-
 }
--- jpo-ode-core/src/main/java/us/dot/its/jpo/ode/wrapper/serdes/MessagingSerializer.java
+++ jpo-ode-core/src/main/java/us/dot/its/jpo/ode/wrapper/serdes/MessagingSerializer.java
@@ -15,28 +15,17 @@
  ******************************************************************************/
 package us.dot.its.jpo.ode.wrapper.serdes;
 
-import java.util.Map;
-
 import org.apache.kafka.common.serialization.Serializer;
-
 import us.dot.its.jpo.ode.util.SerializationUtils;
 
-public class MessagingSerializer<T> implements Serializer<T> {
+public sealed class MessagingSerializer<T> implements Serializer<T>
+    permits OdeBsmSerializer, OdeTimSerializer {
 
-    SerializationUtils<T> serializer = new SerializationUtils<T>();
-
-    @Override
-    public void configure(Map<String, ?> configs, boolean isKey) {
-    }
+    SerializationUtils<T> serializer = new SerializationUtils<>();
 
     @Override
     public byte[] serialize(String topic, T data) {
         return serializer.serialize(data);
-    }
-
-    @Override
-    public void close() {
-        // nothing to do
     }
 
 }
