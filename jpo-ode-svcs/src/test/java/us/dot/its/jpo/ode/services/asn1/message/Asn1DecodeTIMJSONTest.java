04b9a80dea4491948a8e11967e3957ec76b7bb3b
Matt Cook <matt.cook@trihydro.com>
2024-12-16 09:30:33 -0700

    test: swapped RawEncodedTIMJsonRouter in for Asn1DecodeTIMJSON in Asn1DecodeTIMJSONTest

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/services/asn1/message/Asn1DecodeTIMJSONTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/services/asn1/message/Asn1DecodeTIMJSONTest.java
@@ -15,11 +15,14 @@ import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
+import us.dot.its.jpo.ode.config.SerializationConfig;
 import us.dot.its.jpo.ode.kafka.KafkaConsumerConfig;
 import us.dot.its.jpo.ode.kafka.OdeKafkaProperties;
+import us.dot.its.jpo.ode.kafka.listeners.asn1.RawEncodedTIMJsonRouter;
 import us.dot.its.jpo.ode.kafka.producer.KafkaProducerConfig;
 import us.dot.its.jpo.ode.kafka.topics.Asn1CoderTopics;
 import us.dot.its.jpo.ode.kafka.topics.RawEncodedJsonTopics;
@@ -30,7 +33,9 @@ import us.dot.its.jpo.ode.udp.controller.UDPReceiverProperties;
 @SpringBootTest(
     classes = {
         KafkaProducerConfig.class,
-        KafkaConsumerConfig.class
+        KafkaConsumerConfig.class,
+        SerializationConfig.class,
+        RawEncodedTIMJsonRouter.class
     },
     properties = {
         "ode.kafka.topics.raw-encoded-json.tim=topic.Asn1DecoderTestTIMJSON",
@@ -45,12 +50,11 @@ import us.dot.its.jpo.ode.udp.controller.UDPReceiverProperties;
 class Asn1DecodeTIMJSONTest {
 
   @Autowired
-  OdeKafkaProperties odeKafkaProperties;
-
-  @Autowired
   Asn1CoderTopics asn1CoderTopics;
   @Autowired
   RawEncodedJsonTopics rawEncodedJsonTopics;
+  @Autowired
+  private KafkaTemplate<String, String> kafkaTemplate;
 
   @Test
   void testProcess() throws JSONException, IOException {
@@ -65,15 +69,12 @@ class Asn1DecodeTIMJSONTest {
     Consumer<String, String> testConsumer = cf.createConsumer();
     embeddedKafka.consumeFromAnEmbeddedTopic(testConsumer, asn1CoderTopics.getDecoderInput());
 
-    Asn1DecodeTIMJSON testDecodeTimJson =
-        new Asn1DecodeTIMJSON(odeKafkaProperties, asn1CoderTopics.getDecoderInput());
-
     var classLoader = getClass().getClassLoader();
     InputStream inputStream = classLoader
         .getResourceAsStream("us/dot/its/jpo/ode/services/asn1/messages/decoder-input-tim.json");
     assert inputStream != null;
     var json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-    testDecodeTimJson.process(json);
+    kafkaTemplate.send(rawEncodedJsonTopics.getTim(), json);
 
     inputStream = classLoader
         .getResourceAsStream("us/dot/its/jpo/ode/services/asn1/messages/expected-tim.xml");
