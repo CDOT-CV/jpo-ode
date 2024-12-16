f90cbfa3acf4cac8a6ef6ca440bed22e49fd1b34
Matt Cook <matt.cook@trihydro.com>
2024-12-16 09:38:05 -0700

    refactor: use align RawEncodedMAPJsonRouter with other router patterns

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/kafka/listeners/asn1/RawEncodedMAPJsonRouter.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/kafka/listeners/asn1/RawEncodedMAPJsonRouter.java
@@ -3,10 +3,10 @@ package us.dot.its.jpo.ode.kafka.listeners.asn1;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.tomcat.util.buf.HexUtils;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -20,20 +20,18 @@ import us.dot.its.jpo.ode.uper.SupportedMessageType;
 import us.dot.its.jpo.ode.uper.UperUtil;
 
 /**
- * The RawEncodedMAPJsonRouter class is a component designed to listen to messages from a
- * specified Kafka topic, decode the ASN.1 encoded data from received JSON, and publish the decoded
- * data to another Kafka topic. This class utilizes the @KafkaListener and @KafkaHandler annotations
- * to process incoming Kafka messages.
+ * A Kafka listener component that processes ASN.1 encoded MAP JSON messages from a specified Kafka
+ * topic. It processes the raw encoded JSON messages and publishes them to be decoded by the ASN.1
+ * codec
  */
 @Slf4j
 @Component
-@KafkaListener(id = "RawEncodedMAPJsonRouter", topics = "${ode.kafka.topics.raw-encoded-json.map}")
 public class RawEncodedMAPJsonRouter {
 
-  private final ObjectMapper objectMapper;
-
-  private final String publishTopic;
+  private final ObjectMapper mapper;
   private final KafkaTemplate<String, OdeObject> kafkaTemplate;
+  private final String publishTopic;
+  private static final SupportedMessageType MESSAGE_TYPE = SupportedMessageType.MAP;
 
   /**
    * Constructor for the RawEncodedMAPJsonRouter class.
@@ -41,37 +39,35 @@ public class RawEncodedMAPJsonRouter {
    * @param kafkaTemplate The KafkaTemplate instance used to publish decoded data to the specified
    *                      Kafka topic.
    * @param publishTopic  The Kafka topic to which the decoded and processed data is published.
-   * @param objectMapper  The ObjectMapper instance used for JSON serialization and
+   * @param mapper        The ObjectMapper instance used for JSON serialization and
    *                      deserialization.
    */
   public RawEncodedMAPJsonRouter(KafkaTemplate<String, OdeObject> kafkaTemplate,
       @Value("${ode.kafka.topics.asn1.decoder-input}") String publishTopic,
-      ObjectMapper objectMapper) {
+      ObjectMapper mapper) {
     this.kafkaTemplate = kafkaTemplate;
     this.publishTopic = publishTopic;
-    this.objectMapper = objectMapper;
+    this.mapper = mapper;
   }
 
   /**
-   * Processes consumed Kafka messages that contain ASN.1 encoded data within JSON format. This
-   * method decodes the message, extracts metadata and payload, and then publishes the decoded data
-   * to a specified Kafka topic.
+   * Consumes and processes Kafka messages containing ASN.1 encoded MAP JSON data. This method
+   * extracts metadata and payload from the JSON message sends it for decoding.
    *
-   * @param consumedData The raw JSON string consumed from the Kafka topic. This string is expected
-   *                     to contain ASN.1 encoded data that needs processing and further
-   *                     publication.
-   * @throws JsonProcessingException    If there is an error in processing the JSON string.
-   * @throws StartFlagNotFoundException If the start flag is not found in the payload during header
-   *                                    stripping.
+   * @param consumerRecord The Kafka consumer record containing the message key and value. The value
+   *                       includes the raw ASN.1 encoded JSON MAP data to be processed.
+   * @throws StartFlagNotFoundException If the start flag for the MAP message type is not found
+   *                                    during payload processing.
+   * @throws JsonProcessingException    If there's an error while processing or deserializing JSON
+   *                                    data.
    */
-  @KafkaHandler
-  public void listen(String consumedData)
+  @KafkaListener(id = "RawEncodedMAPJsonRouter", topics = "${ode.kafka.topics.raw-encoded-json.map}")
+  public void listen(ConsumerRecord<String, String> consumerRecord)
       throws JsonProcessingException, StartFlagNotFoundException {
-    log.debug("consumedData: {}", consumedData);
-    JSONObject rawMapJsonObject = new JSONObject(consumedData);
+    JSONObject rawMapJsonObject = new JSONObject(consumerRecord.value());
 
     String jsonStringMetadata = rawMapJsonObject.get("metadata").toString();
-    OdeMapMetadata metadata = objectMapper.readValue(jsonStringMetadata, OdeMapMetadata.class);
+    OdeMapMetadata metadata = mapper.readValue(jsonStringMetadata, OdeMapMetadata.class);
 
     Asn1Encoding unsecuredDataEncoding =
         new Asn1Encoding("unsecuredData", "MessageFrame", Asn1Encoding.EncodingRule.UPER);
@@ -81,11 +77,11 @@ public class RawEncodedMAPJsonRouter {
         ((JSONObject) ((JSONObject) rawMapJsonObject.get("payload")).get("data")).getString(
             "bytes");
     payloadHexString =
-        UperUtil.stripDot2Header(payloadHexString, SupportedMessageType.MAP.getStartFlag());
+        UperUtil.stripDot2Header(payloadHexString, MESSAGE_TYPE.getStartFlag());
 
     OdeAsn1Payload payload = new OdeAsn1Payload(HexUtils.fromHexString(payloadHexString));
 
     OdeAsn1Data data = new OdeAsn1Data(metadata, payload);
-    kafkaTemplate.send(publishTopic, data);
+    kafkaTemplate.send(publishTopic, consumerRecord.key(), data);
   }
 }
