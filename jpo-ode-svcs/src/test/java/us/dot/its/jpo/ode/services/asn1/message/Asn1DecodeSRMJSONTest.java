8ad74f1b59ceee86db49397acfb83f3d1711a033
Matt Cook <matt.cook@trihydro.com>
2024-12-16 09:13:10 -0700

    test: update Asn1DecodeSRMJSONTest to confirm data outputted to topic

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/services/asn1/message/Asn1DecodeSRMJSONTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/services/asn1/message/Asn1DecodeSRMJSONTest.java
@@ -2,27 +2,47 @@ package us.dot.its.jpo.ode.services.asn1.message;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.apache.kafka.clients.admin.NewTopic;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import us.dot.its.jpo.ode.config.SerializationConfig;
+import us.dot.its.jpo.ode.kafka.KafkaConsumerConfig;
 import us.dot.its.jpo.ode.kafka.OdeKafkaProperties;
+import us.dot.its.jpo.ode.kafka.producer.KafkaProducerConfig;
 import us.dot.its.jpo.ode.kafka.topics.Asn1CoderTopics;
-import us.dot.its.jpo.ode.model.Asn1Encoding.EncodingRule;
-import us.dot.its.jpo.ode.model.OdeAsn1Data;
-import us.dot.its.jpo.ode.model.OdeAsn1Payload;
-import us.dot.its.jpo.ode.model.OdeSrmMetadata;
+import us.dot.its.jpo.ode.kafka.topics.RawEncodedJsonTopics;
 import us.dot.its.jpo.ode.test.utilities.EmbeddedKafkaHolder;
+import us.dot.its.jpo.ode.udp.controller.UDPReceiverProperties;
 
-@SuppressWarnings({"checkstyle:abbreviationAsWordInName", "checkstyle:linelength"})
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(initializers = ConfigDataApplicationContextInitializer.class)
-@EnableConfigurationProperties(value = {OdeKafkaProperties.class, Asn1CoderTopics.class})
+@SpringBootTest(
+    classes = {
+        KafkaProducerConfig.class,
+        KafkaConsumerConfig.class,
+        SerializationConfig.class
+    },
+    properties = {
+        "ode.kafka.topics.raw-encoded-json.srm=topic.Asn1DecoderTestSRMJSON",
+        "ode.kafka.topics.asn1.decoder-input=topic.Asn1DecoderSRMInput"
+    })
+@EnableConfigurationProperties
+@ContextConfiguration(classes = {
+    UDPReceiverProperties.class, OdeKafkaProperties.class,
+    RawEncodedJsonTopics.class, KafkaProperties.class, Asn1CoderTopics.class
+})
+@DirtiesContext
 class Asn1DecodeSRMJSONTest {
 
   @Autowired
@@ -30,37 +50,38 @@ class Asn1DecodeSRMJSONTest {
 
   @Autowired
   Asn1CoderTopics asn1CoderTopics;
+  @Autowired
+  RawEncodedJsonTopics rawEncodedJsonTopics;
 
   @Test
-  void testProcess() throws JSONException {
+  void testProcess() throws JSONException, IOException {
     var embeddedKafka = EmbeddedKafkaHolder.getEmbeddedKafka();
-    try {
-      embeddedKafka.addTopics(new NewTopic(asn1CoderTopics.getDecoderInput(), 1, (short) 1));
-    } catch (Exception e) {
-      // ignore because we only care that the topic exists not that it was created in this test.
-      // this test doesn't actually consume anything from the queue, it just needs the kafka broker running
-      // and configured with the decoder input topic so that it can verify the message produced to the topic
-    }
+    EmbeddedKafkaHolder.addTopics(asn1CoderTopics.getDecoderInput(), rawEncodedJsonTopics.getSrm());
 
     Asn1DecodeSRMJSON testDecodeSrmJson =
         new Asn1DecodeSRMJSON(odeKafkaProperties, asn1CoderTopics.getDecoderInput());
 
-    String json =
-        "{\"metadata\":{\"recordType\":\"srmTx\",\"securityResultCode\":\"success\",\"payloadType\":\"us.dot.its.jpo.ode.model.OdeAsn1Payload\",\"serialId\":{\"streamId\":\"190cbd65-d1e2-488a-ba42-b7d3f03a5c69\",\"bundleSize\":1,\"bundleId\":0,\"recordId\":0,\"serialNumber\":0},\"odeReceivedAt\":\"2024-03-15T19:24:04.113614500Z\",\"schemaVersion\":6,\"maxDurationTime\":0,\"recordGeneratedBy\":\"OBU\",\"sanitized\":false,\"originIp\":\"192.168.0.1\",\"srmSource\":\"RSU\"},\"payload\":{\"dataType\":\"us.dot.its.jpo.ode.model.OdeHexByteArray\",\"data\":{\"bytes\":\"0381004003807C001D2130000010090BD341080D00855C6C0C6899853000A534F7C24CB29897694759B7C000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\"}}}";
-    OdeAsn1Data resultOdeObj = testDecodeSrmJson.process(json);
+    Map<String, Object> consumerProps =
+        KafkaTestUtils.consumerProps("Asn1DecodeSRMJSONTestConsumer", "false", embeddedKafka);
+    var cf =
+        new DefaultKafkaConsumerFactory<>(consumerProps,
+            new StringDeserializer(), new StringDeserializer());
+    Consumer<String, String> testConsumer = cf.createConsumer();
+    embeddedKafka.consumeFromAnEmbeddedTopic(testConsumer, asn1CoderTopics.getDecoderInput());
 
-    // Validate the metadata
-    OdeSrmMetadata jsonMetadataObj = (OdeSrmMetadata) resultOdeObj.getMetadata();
-    assertEquals(OdeSrmMetadata.SrmSource.RSU, jsonMetadataObj.getSrmSource());
-    assertEquals("unsecuredData", jsonMetadataObj.getEncodings().getFirst().getElementName());
-    assertEquals("MessageFrame", jsonMetadataObj.getEncodings().getFirst().getElementType());
-    assertEquals(EncodingRule.UPER, jsonMetadataObj.getEncodings().getFirst().getEncodingRule());
+    var classLoader = getClass().getClassLoader();
+    InputStream inputStream = classLoader
+        .getResourceAsStream("us/dot/its/jpo/ode/services/asn1/messages/decoder-input-srm.json");
+    assert inputStream != null;
+    var json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+    testDecodeSrmJson.process(json);
 
-    // Validate the payload
-    String expectedPayload =
-        "{\"bytes\":\"001D2130000010090BD341080D00855C6C0C6899853000A534F7C24CB29897694759B7C000\"}";
-    OdeAsn1Payload jsonPayloadObj = (OdeAsn1Payload) resultOdeObj.getPayload();
-    assertEquals("us.dot.its.jpo.ode.model.OdeHexByteArray", jsonPayloadObj.getDataType());
-    assertEquals(expectedPayload, jsonPayloadObj.getData().toString());
+    inputStream = classLoader
+        .getResourceAsStream("us/dot/its/jpo/ode/services/asn1/messages/expected-srm.xml");
+    assert inputStream != null;
+    var expectedSrm = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+
+    var producedSrm = KafkaTestUtils.getSingleRecord(testConsumer, asn1CoderTopics.getDecoderInput());
+    assertEquals(expectedSrm, producedSrm.value());
   }
 }
