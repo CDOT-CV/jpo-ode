c7b893823b19b9aab435eb704e762922cd77ed0d
Matt Cook <matt.cook@trihydro.com>
2024-12-24 14:19:58 -0700

    style: add missing Javadocs to RsuDepositorConfig

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/rsu/RsuDepositorConfig.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/rsu/RsuDepositorConfig.java
@@ -4,9 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import us.dot.its.jpo.ode.security.SecurityServicesProperties;
 
+/**
+ * Configuration class responsible for creating and configuring an instance of RsuDepositor.
+ *
+ * <p>This class sets up the necessary bean for RsuDepositor, ensuring its lifecycle
+ * management is handled by the Spring framework. The RsuDepositor is initialized
+ * with the required properties and started to begin its operations.</p>
+ */
 @Configuration
 public class RsuDepositorConfig {
 
+  /**
+   * Creates and configures an instance of {@link RsuDepositor} as a Spring-managed bean.
+   * The RsuDepositor is initialized with the provided properties and started immediately.
+   *
+   * @param rsuProperties the configuration properties for RSU, including such values
+   *                      as username, password, and the number of "store and repeat" message slots.
+   * @param securityServicesProps the configuration properties for security services,
+   *                               used to determine if RSU signing is enabled.
+   * @return an initialized and running instance of {@link RsuDepositor}.
+   */
   @Bean
   public RsuDepositor rsuDepositor(RsuProperties rsuProperties, SecurityServicesProperties securityServicesProps) {
     var rsuDepositor = new RsuDepositor(rsuProperties, securityServicesProps.getIsRsuSigningEnabled());
