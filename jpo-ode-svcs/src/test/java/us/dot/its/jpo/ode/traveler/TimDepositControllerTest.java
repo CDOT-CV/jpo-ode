dd12c24b73785e38d042cf7ad2acd02424b20eef
dmccoystephenson <dmccoystephenson@gmail.com>
2025-01-03 12:58:25 -0700

    Updated TimDepositControllerTest.testSuccessfulSdwRequestMessageReturnsSuccessMessagePost to verify contents of published records
    Signed-off-by: dmccoystephenson <dmccoystephenson@gmail.com>

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/traveler/TimDepositControllerTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/traveler/TimDepositControllerTest.java
@@ -403,19 +403,19 @@ class TimDepositControllerTest {
     Assertions.assertNotNull(pojoTimBroadcastRecord.value());
     var jsonTimBroadcastRecord =
         KafkaTestUtils.getSingleRecord(jsonTimBroadcastConsumer, jsonTopics.getTimBroadcast());
-    Assertions.assertNotNull(
-        jsonTimBroadcastRecord.value()); // TODO: verify message contents instead of just existence
+    verifyMessageContentsJson("successfulSdwRequestMessageReturnsSuccessMessagePost_timBroadcast_expected.json",
+        jsonTimBroadcastRecord.value());
     var jsonJ2735TimBroadcastRecord = KafkaTestUtils.getSingleRecord(jsonJ2735TimBroadcastConsumer,
         jsonTopics.getJ2735TimBroadcast());
-    Assertions.assertNotNull(
-        jsonJ2735TimBroadcastRecord.value()); // TODO: verify message contents instead of just existence
+    verifyMessageContentsJson("successfulSdwRequestMessageReturnsSuccessMessagePost_j2735TimBroadcast_expected.json",
+        jsonJ2735TimBroadcastRecord.value());
     var jsonTimRecord = KafkaTestUtils.getSingleRecord(jsonTimConsumer, jsonTopics.getTim());
-    Assertions.assertNotNull(
-        jsonTimRecord.value()); // TODO: verify message contents instead of just existence
+    verifyMessageContentsJson("successfulSdwRequestMessageReturnsSuccessMessagePost_tim_expected.json",
+        jsonTimRecord.value());
     var asn1CoderEncoderInputRecord = KafkaTestUtils.getSingleRecord(asn1CoderEncoderInputConsumer,
         asn1CoderTopics.getEncoderInput());
-    Assertions.assertNotNull(
-        asn1CoderEncoderInputRecord.value()); // TODO: verify message contents instead of just existence
+    verifyMessageContentsXml("successfulSdwRequestMessageReturnsSuccessMessagePost_encoderInput_expected.xml",
+        asn1CoderEncoderInputRecord.value());
 
     // cleanup
     pojoTimBroadcastConsumer.close();
