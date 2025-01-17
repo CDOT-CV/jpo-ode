14058e4a13f65d2a3edf1f6aac28e6e0e409554a
Matt Cook <matt.cook@trihydro.com>
2025-01-17 15:08:23 -0700

    docs: add javadocs to LogFileParserFactory

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/importer/parser/LogFileParserFactory.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/importer/parser/LogFileParserFactory.java
@@ -3,9 +3,19 @@ package us.dot.its.jpo.ode.importer.parser;
 import lombok.extern.slf4j.Slf4j;
 import us.dot.its.jpo.ode.model.OdeLogMetadata;
 
+/**
+ * Factory class for creating instances of specific log file parsers based on the
+ * file name prefix. This factory helps in determining and returning the appropriate
+ * parser implementation for processing different types of log files.
+ */
 @Slf4j
 public class LogFileParserFactory {
 
+  /**
+   * Exception class representing errors that occur during the creation of log file parsers
+   * in the {@code LogFileParserFactory} class. This exception is thrown when a log file with
+   * an unrecognized or unsupported prefix is encountered.
+   */
   public static class LogFileParserFactoryException extends Exception {
     public LogFileParserFactoryException(String message) {
       super(message);
@@ -16,6 +26,19 @@ public class LogFileParserFactory {
     throw new UnsupportedOperationException();
   }
 
+  /**
+   * Retrieves an appropriate instance of a log file parser based on the given file name prefix.
+   * Determines the file type by matching the prefix of the file name with predefined record types and
+   * creates an instance of the corresponding parser for that type.
+   *
+   * @param fileName the name of the log file, including its prefix, which will be used to
+   *                 identify the appropriate parser type
+   *
+   * @return an instance of a subclass of {@link LogFileParser} that matches the file type
+   *
+   * @throws LogFileParserFactoryException if the file name prefix does not match any supported
+   *                                       record type
+   */
   public static LogFileParser getLogFileParser(String fileName) throws LogFileParserFactoryException {
     LogFileParser fileParser;
     if (fileName.startsWith(OdeLogMetadata.RecordType.bsmTx.name())) {
