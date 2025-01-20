3c12d2084ea81aae5df0e687bba39aa0430cc9c0
Matt Cook <matt.cook@trihydro.com>
2025-01-20 16:15:15 -0700

    style: update style and javadoc in LogFileToAsn1CodecPublisher

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/coder/stream/LogFileToAsn1CodecPublisher.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/coder/stream/LogFileToAsn1CodecPublisher.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*=============================================================================
  * Copyright 2018 572682
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
@@ -13,6 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
+
 package us.dot.its.jpo.ode.coder.stream;
 
 import java.io.BufferedInputStream;
@@ -45,144 +46,188 @@ import us.dot.its.jpo.ode.model.SerialId;
 import us.dot.its.jpo.ode.uper.UperUtil;
 import us.dot.its.jpo.ode.util.JsonUtils;
 
+/**
+ * The LogFileToAsn1CodecPublisher class is responsible for processing log files
+ * and publishing data in ASN.1 encoded format to Kafka topics. It serves as an implementation
+ * of the Asn1CodecPublisher interface.
+ *
+ * <p>This class handles different file types and decoding processes, converting them into ODE metadata
+ * and payload models for publishing. It can also differentiate between various record types, such as
+ * Driver Alert, BSM (Basic Safety Message), and SPAT (Signal Phase and Timing), and directs the data
+ * to the appropriate topics defined in RawEncodedJsonTopics or JsonTopics.</p>
+ */
 @Slf4j
 public class LogFileToAsn1CodecPublisher implements Asn1CodecPublisher {
 
-    public static class LogFileToAsn1CodecPublisherException extends Exception {
+  /**
+   * This exception is thrown to indicate an error that occurred while processing
+   * a log file to be published via the ASN.1 codec. It acts as a wrapper for
+   * exceptions that arise in this context, providing additional context through
+   * a descriptive message and the underlying cause.
+   *
+   * <p>The exception is typically used in scenarios where log files are parsed,
+   * converted, or otherwise processed to be published in ASN.1 format, and a
+   * failure in this process needs appropriate propagation and handling.</p>
+   */
+  public static class LogFileToAsn1CodecPublisherException extends Exception {
 
-        private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-        public LogFileToAsn1CodecPublisherException(String string, Exception e) {
-            super(string, e);
-        }
-
+    public LogFileToAsn1CodecPublisherException(String string, Exception e) {
+      super(string, e);
     }
 
-    private final RawEncodedJsonTopics rawEncodedJsonTopics;
-    private final JsonTopics jsonTopics;
-    protected StringPublisher publisher;
-    protected LogFileParser fileParser;
-    protected SerialId serialId;
+  }
 
-    public LogFileToAsn1CodecPublisher(StringPublisher stringPublisher, JsonTopics jsonTopics, RawEncodedJsonTopics rawEncodedJsonTopics) {
-        this.jsonTopics = jsonTopics;
-        this.rawEncodedJsonTopics = rawEncodedJsonTopics;
-        this.publisher = stringPublisher;
-        this.serialId = new SerialId();
-    }
+  private final RawEncodedJsonTopics rawEncodedJsonTopics;
+  private final JsonTopics jsonTopics;
+  protected StringPublisher publisher;
+  protected LogFileParser fileParser;
+  protected SerialId serialId;
 
-    public List<OdeData> publish(BufferedInputStream inputStream, String fileName, ImporterFileType fileType)
-        throws LogFileToAsn1CodecPublisherException, LogFileParserFactory.LogFileParserFactoryException {
-        ParserStatus status;
+  /**
+   * Constructs a LogFileToAsn1CodecPublisher instance used for converting log files
+   * to ASN.1 encoded formats and publishing them. It integrates with given
+   * publishers and topic configurations for data processing and dissemination.
+   *
+   * @param stringPublisher      the publisher responsible for publishing the encoded strings
+   * @param jsonTopics           the topic configuration for processing JSON data
+   * @param rawEncodedJsonTopics the topic configuration for processing raw encoded JSON data
+   */
+  public LogFileToAsn1CodecPublisher(StringPublisher stringPublisher, JsonTopics jsonTopics, RawEncodedJsonTopics rawEncodedJsonTopics) {
+    this.jsonTopics = jsonTopics;
+    this.rawEncodedJsonTopics = rawEncodedJsonTopics;
+    this.publisher = stringPublisher;
+    this.serialId = new SerialId();
+  }
 
-        List<OdeData> dataList = new ArrayList<>();
-        if (fileType == ImporterFileType.LOG_FILE) {
-            fileParser = LogFileParserFactory.getLogFileParser(fileName);
+  /**
+   * Publishes data parsed from a given input stream based on the specified file type.
+   * The method processes the input stream, parses the data using specific parsers
+   * depending on the file type, and publishes the parsed data. Handles both complete
+   * and incomplete data scenarios and maintains a list of all successfully parsed data.
+   *
+   * @param inputStream the input stream containing the data to be parsed
+   * @param fileName    the name of the file associated with the data in the input stream
+   * @param fileType    the type of file being processed, which determines the parser to be used
+   *
+   * @return a list of {@code OdeData} objects that have been successfully parsed
+   *
+   * @throws LogFileToAsn1CodecPublisherException               if an error occurs while parsing or publishing the data
+   * @throws LogFileParserFactory.LogFileParserFactoryException if an error occurs while creating the file parser
+   */
+  public List<OdeData> publish(BufferedInputStream inputStream, String fileName, ImporterFileType fileType)
+      throws LogFileToAsn1CodecPublisherException, LogFileParserFactory.LogFileParserFactoryException {
+    ParserStatus status;
 
-            do {
-                try {
-                    status = fileParser.parseFile(inputStream, fileName);
-                    switch (status) {
-                        case ParserStatus.COMPLETE -> addDataToList(dataList);
-                        case ParserStatus.EOF -> publishList(dataList);
-                        case ParserStatus.INIT -> log.error("Failed to parse the header bytes.");
-                        default -> log.error("Failed to decode ASN.1 data");
-                    }
-                    inputStream = removeNextNewLineCharacter(inputStream);
-                } catch (Exception e) {
-                    throw new LogFileToAsn1CodecPublisherException("Error parsing or publishing data.", e);
-                }
-            } while (status == ParserStatus.COMPLETE);
-        }
+    List<OdeData> dataList = new ArrayList<>();
+    if (fileType == ImporterFileType.LOG_FILE) {
+      fileParser = LogFileParserFactory.getLogFileParser(fileName);
 
-        return dataList;
-    }
-
-    private void addDataToList(List<OdeData> dataList) {
-
-        OdeData odeData;
-
-        OdeMsgPayload payload;
-        OdeLogMetadata metadata;
-        if (isDriverAlertRecord()) {
-            payload = new OdeDriverAlertPayload(((DriverAlertFileParser) fileParser).getAlert());
-            metadata = new OdeLogMetadata(payload);
-            odeData = new OdeDriverAlertData(metadata, payload);
-        } else if (isBsmRecord()) {
-            payload = new OdeAsn1Payload(fileParser.getPayloadParser().getPayload());
-            metadata = new OdeBsmMetadata(payload);
-            odeData = new OdeAsn1Data(metadata, payload);
-        } else if (isSpatRecord()) {
-            payload = new OdeAsn1Payload(fileParser.getPayloadParser().getPayload());
-            metadata = new OdeSpatMetadata(payload);
-            odeData = new OdeAsn1Data(metadata, payload);
-        } else {
-            payload = new OdeAsn1Payload(fileParser.getPayloadParser().getPayload());
-            metadata = new OdeLogMetadata(payload);
-            odeData = new OdeAsn1Data(metadata, payload);
-        }
-        fileParser.updateMetadata(metadata);
-
-        dataList.add(odeData);
-    }
-
-    public boolean isDriverAlertRecord() {
-        return fileParser instanceof DriverAlertFileParser;
-    }
-
-    public boolean isBsmRecord() {
-        return fileParser instanceof BsmLogFileParser || (fileParser instanceof RxMsgFileParser
-                && ((RxMsgFileParser) fileParser).getRxSource() == RxSource.RV);
-    }
-
-    public boolean isSpatRecord() {
-        return fileParser instanceof SpatLogFileParser;
-    }
-
-    private void publishList(List<OdeData> dataList) {
-        serialId.setBundleSize(dataList.size());
-
-        for (OdeData odeData : dataList) {
-            OdeLogMetadata msgMetadata = (OdeLogMetadata) odeData.getMetadata();
-            OdeMsgPayload msgPayload = odeData.getPayload();
-            msgMetadata.setSerialId(serialId);
-
-            if (isDriverAlertRecord()) {
-                publisher.publish(jsonTopics.getDriverAlert(), JsonUtils.toJson(odeData, false));
-            } else if (isBsmRecord()) {
-                publisher.publish(rawEncodedJsonTopics.getBsm(), JsonUtils.toJson(odeData, false));
-            } else if (isSpatRecord()) {
-                publisher.publish(rawEncodedJsonTopics.getSpat(), JsonUtils.toJson(odeData, false));
-            } else {
-                String messageType = UperUtil.determineMessageType(msgPayload);
-                switch (messageType) {
-                    case "MAP" -> publisher.publish(rawEncodedJsonTopics.getMap(), JsonUtils.toJson(odeData, false));
-                    case "SPAT" -> publisher.publish(rawEncodedJsonTopics.getSpat(), JsonUtils.toJson(odeData, false));
-                    case "TIM" -> publisher.publish(rawEncodedJsonTopics.getTim(), JsonUtils.toJson(odeData, false));
-                    case "BSM" -> publisher.publish(rawEncodedJsonTopics.getBsm(), JsonUtils.toJson(odeData, false));
-                    case "SSM" -> publisher.publish(rawEncodedJsonTopics.getSsm(), JsonUtils.toJson(odeData, false));
-                    case "SRM" -> publisher.publish(rawEncodedJsonTopics.getSrm(), JsonUtils.toJson(odeData, false));
-                    case "PSM" -> publisher.publish(rawEncodedJsonTopics.getPsm(), JsonUtils.toJson(odeData, false));
-                    default -> log.warn("Unknown message type: {}", messageType);
-                }
-            }
-
-            serialId.increment();
-        }
-    }
-
-    // This method will check if the next character is a newline character (0x0A in hex or 10 in converted decimal)
-    // or if the next character does not contain a newline character it will put that character back into the buffered input stream
-    public BufferedInputStream removeNextNewLineCharacter(BufferedInputStream bis) {
+      do {
         try {
-            bis.mark(1);
-            int nextByte = bis.read();
-            if (nextByte != 10) { // If the next byte is not a newline
-                bis.reset(); // Reset the stream back to the most recent mark
-            }
-        } catch (IOException e) {
-            log.error("Error removing next newline character: ", e);
+          status = fileParser.parseFile(inputStream, fileName);
+          switch (status) {
+            case ParserStatus.COMPLETE -> addDataToList(dataList);
+            case ParserStatus.EOF -> publishList(dataList);
+            case ParserStatus.INIT -> log.error("Failed to parse the header bytes.");
+            default -> log.error("Failed to decode ASN.1 data");
+          }
+          inputStream = removeNextNewLineCharacter(inputStream);
+        } catch (Exception e) {
+          throw new LogFileToAsn1CodecPublisherException("Error parsing or publishing data.", e);
         }
-        return bis;
+      } while (status == ParserStatus.COMPLETE);
     }
+
+    return dataList;
+  }
+
+  private void addDataToList(List<OdeData> dataList) {
+
+    OdeData odeData;
+
+    OdeMsgPayload payload;
+    OdeLogMetadata metadata;
+    if (isDriverAlertRecord()) {
+      payload = new OdeDriverAlertPayload(((DriverAlertFileParser) fileParser).getAlert());
+      metadata = new OdeLogMetadata(payload);
+      odeData = new OdeDriverAlertData(metadata, payload);
+    } else if (isBsmRecord()) {
+      payload = new OdeAsn1Payload(fileParser.getPayloadParser().getPayload());
+      metadata = new OdeBsmMetadata(payload);
+      odeData = new OdeAsn1Data(metadata, payload);
+    } else if (isSpatRecord()) {
+      payload = new OdeAsn1Payload(fileParser.getPayloadParser().getPayload());
+      metadata = new OdeSpatMetadata(payload);
+      odeData = new OdeAsn1Data(metadata, payload);
+    } else {
+      payload = new OdeAsn1Payload(fileParser.getPayloadParser().getPayload());
+      metadata = new OdeLogMetadata(payload);
+      odeData = new OdeAsn1Data(metadata, payload);
+    }
+    fileParser.updateMetadata(metadata);
+
+    dataList.add(odeData);
+  }
+
+  public boolean isDriverAlertRecord() {
+    return fileParser instanceof DriverAlertFileParser;
+  }
+
+  public boolean isBsmRecord() {
+    return fileParser instanceof BsmLogFileParser || (fileParser instanceof RxMsgFileParser
+        && ((RxMsgFileParser) fileParser).getRxSource() == RxSource.RV);
+  }
+
+  public boolean isSpatRecord() {
+    return fileParser instanceof SpatLogFileParser;
+  }
+
+  private void publishList(List<OdeData> dataList) {
+    serialId.setBundleSize(dataList.size());
+
+    for (OdeData odeData : dataList) {
+      OdeLogMetadata msgMetadata = (OdeLogMetadata) odeData.getMetadata();
+      OdeMsgPayload msgPayload = odeData.getPayload();
+      msgMetadata.setSerialId(serialId);
+
+      if (isDriverAlertRecord()) {
+        publisher.publish(jsonTopics.getDriverAlert(), JsonUtils.toJson(odeData, false));
+      } else if (isBsmRecord()) {
+        publisher.publish(rawEncodedJsonTopics.getBsm(), JsonUtils.toJson(odeData, false));
+      } else if (isSpatRecord()) {
+        publisher.publish(rawEncodedJsonTopics.getSpat(), JsonUtils.toJson(odeData, false));
+      } else {
+        String messageType = UperUtil.determineMessageType(msgPayload);
+        switch (messageType) {
+          case "MAP" -> publisher.publish(rawEncodedJsonTopics.getMap(), JsonUtils.toJson(odeData, false));
+          case "SPAT" -> publisher.publish(rawEncodedJsonTopics.getSpat(), JsonUtils.toJson(odeData, false));
+          case "TIM" -> publisher.publish(rawEncodedJsonTopics.getTim(), JsonUtils.toJson(odeData, false));
+          case "BSM" -> publisher.publish(rawEncodedJsonTopics.getBsm(), JsonUtils.toJson(odeData, false));
+          case "SSM" -> publisher.publish(rawEncodedJsonTopics.getSsm(), JsonUtils.toJson(odeData, false));
+          case "SRM" -> publisher.publish(rawEncodedJsonTopics.getSrm(), JsonUtils.toJson(odeData, false));
+          case "PSM" -> publisher.publish(rawEncodedJsonTopics.getPsm(), JsonUtils.toJson(odeData, false));
+          default -> log.warn("Unknown message type: {}", messageType);
+        }
+      }
+
+      serialId.increment();
+    }
+  }
+
+  // This method will check if the next character is a newline character (0x0A in hex or 10 in converted decimal)
+  // or if the next character does not contain a newline character it will put that character back into the buffered input stream
+  private BufferedInputStream removeNextNewLineCharacter(BufferedInputStream bis) {
+    try {
+      bis.mark(1);
+      int nextByte = bis.read();
+      if (nextByte != 10) { // If the next byte is not a newline
+        bis.reset(); // Reset the stream back to the most recent mark
+      }
+    } catch (IOException e) {
+      log.error("Error removing next newline character: ", e);
+    }
+    return bis;
+  }
 }
