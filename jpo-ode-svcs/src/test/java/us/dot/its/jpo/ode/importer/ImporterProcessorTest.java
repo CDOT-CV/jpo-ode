a66e8439256810a9ed6f0c1c52173f2dc8635b63
Matt Cook <matt.cook@trihydro.com>
2025-01-24 10:54:35 -0700

    test: define test cases for ImporterProcessorTest

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/importer/ImporterProcessorTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/importer/ImporterProcessorTest.java
@@ -1,102 +1,49 @@
-/*******************************************************************************
+/*=============================================================================
  * Copyright 2018 572682
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
  * License for the specific language governing permissions and limitations under
  * the License.
  ******************************************************************************/
+
 package us.dot.its.jpo.ode.importer;
 
-import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Iterator;
-import mockit.Expectations;
-import mockit.Injectable;
-import mockit.Mocked;
-import mockit.Tested;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import us.dot.its.jpo.ode.importer.ImporterDirectoryWatcher.ImporterFileType;
 
-public class ImporterProcessorTest {
+class ImporterProcessorTest {
 
-   @Tested
-   ImporterProcessor testImporterProcessor;
+  @Test
+  void processDirectory_GZIP() {
+    fail("Not yet implemented");
+  }
 
-   @Injectable
-   ImporterFileType injectableImporterDirType = ImporterFileType.LOG_FILE;
+  @Test
+  void processDirectory_ZIP() {
+    fail("Not yet implemented");
+  }
 
-//   @Capturing
-//   FileAsn1CodecPublisher capturingFileAsn1CodecPublisher;
-//   @Capturing
-//   OdeFileUtils capturingOdeFileUtils;
+  @Test
+  void processDirectory_UNKNOWN() {
+    fail("Not yet implemented");
+  }
 
+  @Test
+  void processDirectory_NestedDirectory() {
+    fail("Not yet implemented");
+  }
 
-
-   @Mocked
-   Path mockFile;
-   @Mocked
-   Path mockFileBackup;
-   
-   @Injectable
-   Path injectableDir;
-   @Injectable
-   Path injectableBackupDir;
-   @Injectable
-   Path injectableFailureDir;
-   
-
-   @Test @Disabled
-   public void processExistingFilesShouldCatchExceptionFailedToCreateStream() {
-
-      try {
-         new Expectations() {
-            {
-               Files.newDirectoryStream((Path) any);
-               result = new IOException("testException123");
-            }
-         };
-      } catch (IOException e) {
-         fail("Unexpected exception in expectations block: " + e);
-      }
-
-      testImporterProcessor.processDirectory(injectableDir, injectableBackupDir, injectableFailureDir);
-   }
- 
-   @Test @Disabled
-   public void processExistingFilesShouldProcessOneFile(@Mocked DirectoryStream<Path> mockDirectoryStream,
-         @Mocked Iterator<Path> mockIterator) {
-
-      try {
-         new Expectations() {
-            {
-               Files.newDirectoryStream((Path) any);
-               result = mockDirectoryStream;
-               mockDirectoryStream.iterator();
-               result = mockIterator;
-               mockIterator.hasNext();
-               returns(true, false);
-               mockIterator.next();
-               returns(mockFile, null);
-            }
-         };
-      } catch (IOException e) {
-         fail("Unexpected exception in expectations block: " + e);
-      }
-
-      testImporterProcessor.processDirectory(injectableDir, injectableBackupDir, injectableFailureDir);
-   }
-
+  @Test
+  void processDirectory_DirectoryWithNoFiles() {
+    fail("Not yet implemented");
+  }
 }
