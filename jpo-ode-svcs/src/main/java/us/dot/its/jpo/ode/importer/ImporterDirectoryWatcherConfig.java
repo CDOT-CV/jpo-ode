757e9c834e0a495b902fa9998a51f75729cc702f
Matt Cook <matt.cook@trihydro.com>
2025-01-27 13:52:47 -0700

    refactor: remove unused OdeKafkaProperties field from ImporterDirectoryWatcherConfig

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/importer/ImporterDirectoryWatcherConfig.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/importer/ImporterDirectoryWatcherConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaTemplate;
 import us.dot.its.jpo.ode.coder.stream.FileImporterProperties;
-import us.dot.its.jpo.ode.kafka.OdeKafkaProperties;
 import us.dot.its.jpo.ode.kafka.topics.JsonTopics;
 import us.dot.its.jpo.ode.kafka.topics.RawEncodedJsonTopics;
 
@@ -23,7 +22,6 @@ public class ImporterDirectoryWatcherConfig {
    *
    * @param fileImporterProps    the file importer properties containing directory paths, buffer size,
    *                             and other configuration details for file processing.
-   * @param odeKafkaProperties   the Kafka configuration properties for producer and topic details.
    * @param jsonTopics           configuration for JSON Kafka topics used during the file processing.
    * @param rawEncodedJsonTopics configuration for raw-encoded JSON Kafka topics used during the
    *                             file processing.
@@ -34,13 +32,11 @@ public class ImporterDirectoryWatcherConfig {
   @Bean
   public ImporterDirectoryWatcher importerDirectoryWatcher(
       FileImporterProperties fileImporterProps,
-      OdeKafkaProperties odeKafkaProperties,
       JsonTopics jsonTopics,
       RawEncodedJsonTopics rawEncodedJsonTopics,
       KafkaTemplate<String, String> kafkaTemplate
   ) {
     return new ImporterDirectoryWatcher(fileImporterProps,
-        odeKafkaProperties,
         jsonTopics,
         ImporterDirectoryWatcher.ImporterFileType.LOG_FILE,
         rawEncodedJsonTopics,
