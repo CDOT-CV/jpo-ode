6f0ef450664ad0d666adbcf4a0dae1e267b4a8a8
Michael7371 <40476797+Michael7371@users.noreply.github.com>
2025-08-14 17:32:59 -0600

    updating spring boot version to 3.5.4 and fixing kafka embedded kafka holder

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/test/utilities/EmbeddedKafkaHolder.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/test/utilities/EmbeddedKafkaHolder.java
@@ -4,28 +4,30 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.kafka.KafkaException;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.EmbeddedKafkaZKBroker;
 
 /**
  * The EmbeddedKafkaHolder class is a utility for managing a singleton instance of an embedded Kafka
  * broker for testing purposes. It ensures that the embedded Kafka broker is started only once
  * throughout the application lifecycle.
  *
- * <p>This class initializes an embedded Kafka broker with a specified configuration, which
- * includes a single broker and auto-start enabled. The broker list property is set to
+ * <p>
+ * This class initializes an embedded Kafka broker with a specified configuration, which includes a
+ * single broker and auto-start enabled. The broker list property is set to
  * "spring.kafka.bootstrap-servers".
  *
- * </p>The embedded Kafka instance is lazily started when the getEmbeddedKafka method is called
- * for the first time. If the broker fails to start, a KafkaException is thrown.
+ * </p>
+ * The embedded Kafka instance is lazily started when the getEmbeddedKafka method is called for the
+ * first time. If the broker fails to start, a KafkaException is thrown.
  *
- * </p>The class is designed to be non-instantiable with a private constructor.
+ * </p>
+ * The class is designed to be non-instantiable with a private constructor.
  */
 @Slf4j
 public final class EmbeddedKafkaHolder {
 
-  private static final EmbeddedKafkaBroker embeddedKafka = new EmbeddedKafkaBroker(1,
-      true,
-      1)
-      .brokerListProperty("spring.kafka.bootstrap-servers");
+  private static EmbeddedKafkaBroker embeddedKafka =
+      new EmbeddedKafkaZKBroker(1, false).brokerListProperty("spring.kafka.bootstrap-servers");
 
   private static boolean started;
 
@@ -39,21 +41,25 @@ public final class EmbeddedKafkaHolder {
    */
   public static EmbeddedKafkaBroker getEmbeddedKafka() {
     if (!started) {
-      try {
-        embeddedKafka.kafkaPorts(4242);
-        embeddedKafka.afterPropertiesSet();
-      } catch (Exception e) {
-        throw new KafkaException("Embedded broker failed to start", e);
+      synchronized (EmbeddedKafkaHolder.class) {
+        if (!started) {
+          try {
+            embeddedKafka.kafkaPorts(4242);
+            embeddedKafka.afterPropertiesSet();
+          } catch (Exception e) {
+            throw new KafkaException("Embedded broker failed to start", e);
+          }
+          started = true;
+        }
       }
-      started = true;
     }
     return embeddedKafka;
   }
 
   /**
-   * Adds one or more topics to the embedded Kafka broker instance. Each topic will be created
-   * with a replication factor of 1 and a single partition. If a topic already exists, no action
-   * is taken, and the exception is ignored.
+   * Adds one or more topics to the embedded Kafka broker instance. Each topic will be created with
+   * a replication factor of 1 and a single partition. If a topic already exists, no action is
+   * taken, and the exception is ignored.
    *
    * @param topics one or more topic names to be added to the embedded Kafka broker
    */
