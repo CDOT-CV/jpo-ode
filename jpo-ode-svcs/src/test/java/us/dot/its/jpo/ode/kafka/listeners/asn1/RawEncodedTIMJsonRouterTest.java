d53aedad70311924d6a7755ffac161de2c4813c5
Paige Monington <pmonington@trihydro.com>
2026-03-26 15:06:17 -0600

    test: refactor RawEncodedTIMJsonRouterTest to use @EmbeddedKafka and @KafkaListener

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/kafka/listeners/asn1/RawEncodedTIMJsonRouterTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/kafka/listeners/asn1/RawEncodedTIMJsonRouterTest.java
@@ -1,24 +1,26 @@
 package us.dot.its.jpo.ode.kafka.listeners.asn1;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import us.dot.its.jpo.ode.config.SerializationConfig;
 import us.dot.its.jpo.ode.kafka.KafkaConsumerConfig;
 import us.dot.its.jpo.ode.kafka.OdeKafkaProperties;
@@ -28,7 +30,6 @@ import us.dot.its.jpo.ode.kafka.listeners.json.RawEncodedTIMJsonRouter;
 import us.dot.its.jpo.ode.kafka.producer.KafkaProducerConfig;
 import us.dot.its.jpo.ode.kafka.topics.Asn1CoderTopics;
 import us.dot.its.jpo.ode.kafka.topics.RawEncodedJsonTopics;
-import us.dot.its.jpo.ode.test.utilities.EmbeddedKafkaHolder;
 import us.dot.its.jpo.ode.udp.controller.UDPReceiverProperties;
 
 
@@ -45,6 +46,8 @@ import us.dot.its.jpo.ode.udp.controller.UDPReceiverProperties;
         "ode.kafka.topics.raw-encoded-json.tim=topic.Asn1DecoderTestTIMJSON",
         "ode.kafka.topics.asn1.decoder-input=topic.Asn1DecoderTIMInput"
     })
+@EmbeddedKafka(partitions = 1, topics = {"topic.Asn1DecoderTestTIMJSON", "topic.Asn1DecoderTIMInput"})
+@TestPropertySource(properties = {"spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}"})
 @EnableConfigurationProperties
 @ContextConfiguration(classes = {
     UDPReceiverProperties.class, OdeKafkaProperties.class,
@@ -60,18 +63,13 @@ class RawEncodedTIMJsonRouterTest {
   @Autowired
   private KafkaTemplate<String, String> kafkaTemplate;
 
-  @Test
-  void testListen() throws JSONException, IOException {
-    var embeddedKafka = EmbeddedKafkaHolder.getEmbeddedKafka();
-    EmbeddedKafkaHolder.addTopics(asn1CoderTopics.getDecoderInput(), rawEncodedJsonTopics.getTim());
+  private CountDownLatch latch;
+  private String odeTimData;
 
-    Map<String, Object> consumerProps =
-        KafkaTestUtils.consumerProps("Asn1DecodeTIMJSONTestConsumer", "false", embeddedKafka);
-    var cf =
-        new DefaultKafkaConsumerFactory<>(consumerProps,
-            new StringDeserializer(), new StringDeserializer());
-    Consumer<String, String> testConsumer = cf.createConsumer();
-    embeddedKafka.consumeFromAnEmbeddedTopic(testConsumer, asn1CoderTopics.getDecoderInput());
+  @Test
+  void testListen() throws JSONException, IOException, InterruptedException {
+    latch = new CountDownLatch(1);
+    odeTimData = null;
 
     var classLoader = getClass().getClassLoader();
     InputStream inputStream = classLoader
@@ -81,14 +79,19 @@ class RawEncodedTIMJsonRouterTest {
     var json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
     kafkaTemplate.send(rawEncodedJsonTopics.getTim(), json);
 
+    assertThat(latch.await(10, TimeUnit.SECONDS)).isTrue();
+
     inputStream = classLoader
         .getResourceAsStream("us/dot/its/jpo/ode/kafka/listeners/asn1/expected-tim.xml");
     assert inputStream != null;
     var expectedTim = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
 
-    var produced =
-        KafkaTestUtils.getSingleRecord(testConsumer, asn1CoderTopics.getDecoderInput());
-    assertEquals(expectedTim, produced.value());
-    testConsumer.close();
+    assertEquals(expectedTim, odeTimData);
+  }
+
+  @KafkaListener(topics = {"topic.Asn1DecoderTIMInput", "topic.Asn1DecoderTestTIMJSON"} , groupId = "test-group")
+  public void receive(String payload) {
+    odeTimData = payload;
+    latch.countDown(); // Decrement the latch once the message is received
   }
 }
