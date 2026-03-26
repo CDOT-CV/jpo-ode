cfed20b3d353e364543c811da363d42f437e90ac
Paige Monington <pmonington@trihydro.com>
2026-03-26 14:41:29 -0600

    test: refactor RawEncodedSRMJsonRouterTest to use EmbeddedKafka and CountDownLatch for message validation

--- jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/kafka/listeners/asn1/RawEncodedSRMJsonRouterTest.java
+++ jpo-ode-svcs/src/test/java/us/dot/its/jpo/ode/kafka/listeners/asn1/RawEncodedSRMJsonRouterTest.java
@@ -1,24 +1,26 @@
 package us.dot.its.jpo.ode.kafka.listeners.asn1;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.kafka.autoconfigure.KafkaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import us.dot.its.jpo.ode.config.SerializationConfig;
 import us.dot.its.jpo.ode.kafka.KafkaConsumerConfig;
 import us.dot.its.jpo.ode.kafka.OdeKafkaProperties;
@@ -28,7 +30,6 @@ import us.dot.its.jpo.ode.kafka.listeners.json.RawEncodedSRMJsonRouter;
 import us.dot.its.jpo.ode.kafka.producer.KafkaProducerConfig;
 import us.dot.its.jpo.ode.kafka.topics.Asn1CoderTopics;
 import us.dot.its.jpo.ode.kafka.topics.RawEncodedJsonTopics;
-import us.dot.its.jpo.ode.test.utilities.EmbeddedKafkaHolder;
 import us.dot.its.jpo.ode.udp.controller.UDPReceiverProperties;
 
 @SpringBootTest(
@@ -44,6 +45,8 @@ import us.dot.its.jpo.ode.udp.controller.UDPReceiverProperties;
         "ode.kafka.topics.raw-encoded-json.srm=topic.Asn1DecoderTestSRMJSON",
         "ode.kafka.topics.asn1.decoder-input=topic.Asn1DecoderSRMInput"
     })
+@EmbeddedKafka(partitions = 1, topics = {"topic.Asn1DecoderTestRSMJSON", "topic.Asn1DecoderSRMInput"})
+@TestPropertySource(properties = {"spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}"})
 @EnableConfigurationProperties
 @ContextConfiguration(classes = {
     UDPReceiverProperties.class, OdeKafkaProperties.class,
@@ -59,18 +62,13 @@ class RawEncodedSRMJsonRouterTest {
   @Autowired
   private KafkaTemplate<String, String> kafkaTemplate;
 
-  @Test
-  void testListen() throws JSONException, IOException {
-    var embeddedKafka = EmbeddedKafkaHolder.getEmbeddedKafka();
-    EmbeddedKafkaHolder.addTopics(asn1CoderTopics.getDecoderInput(), rawEncodedJsonTopics.getSrm());
+  private CountDownLatch latch;
+  private String odeRsmData;
 
-    Map<String, Object> consumerProps =
-        KafkaTestUtils.consumerProps("Asn1DecodeSRMJSONTestConsumer", "false", embeddedKafka);
-    var cf =
-        new DefaultKafkaConsumerFactory<>(consumerProps,
-            new StringDeserializer(), new StringDeserializer());
-    Consumer<String, String> testConsumer = cf.createConsumer();
-    embeddedKafka.consumeFromAnEmbeddedTopic(testConsumer, asn1CoderTopics.getDecoderInput());
+  @Test
+  void testListen() throws JSONException, IOException, InterruptedException {
+    latch = new CountDownLatch(1);
+    odeRsmData = null;
 
     var classLoader = getClass().getClassLoader();
     InputStream inputStream = classLoader
@@ -80,13 +78,19 @@ class RawEncodedSRMJsonRouterTest {
     var json = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
     kafkaTemplate.send(rawEncodedJsonTopics.getSrm(), json);
 
+    assertThat(latch.await(10, TimeUnit.SECONDS)).isTrue();
+
     inputStream = classLoader
         .getResourceAsStream("us/dot/its/jpo/ode/kafka/listeners/asn1/expected-srm.xml");
     assert inputStream != null;
     var expectedSrm = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
 
-    var producedSrm = KafkaTestUtils.getSingleRecord(testConsumer, asn1CoderTopics.getDecoderInput());
-    assertEquals(expectedSrm, producedSrm.value());
-    testConsumer.close();
+    assertEquals(expectedSrm, odeRsmData);
   }
+
+    @KafkaListener(topics = {"topic.Asn1DecoderSRMInput", "topic.Asn1DecoderTestRSMJSON"} , groupId = "test-group")
+    public void receive(String payload) {
+        odeRsmData = payload;
+        latch.countDown(); // Decrement the latch once the message is received
+    }
 }
