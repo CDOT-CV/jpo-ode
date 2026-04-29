a3a1b7f4065b0491fda6e772b972c5782bac68fd
Matt Cook <matt.cook@trihydro.com>
2026-04-29 13:18:49 -0600

    style(heartbeat): normalize RsuHealthController to 2-space indentation
    Fixes checkstyle IndentationCheck violations flagged by reviewdog on PR
    #229. The try-with-resources refactor introduced 4-space indentation for
    the try body; Google style (basicOffset=2) requires 2 spaces per level.
    Reformatted the entire class for consistency and split the overlong
    method signature onto separate lines.

--- jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/heartbeat/RsuHealthController.java
+++ jpo-ode-svcs/src/main/java/us/dot/its/jpo/ode/heartbeat/RsuHealthController.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2018 572682
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.  You may obtain a copy
  * of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -38,50 +38,55 @@ import us.dot.its.jpo.ode.util.CodecUtils;
 @RestController
 public class RsuHealthController {
 
-    private RsuHealthController() {
+  private RsuHealthController() {
+  }
+
+  @GetMapping(value = "/rsuHeartbeat", produces = "application/json")
+  @ResponseBody
+  public static String heartBeat(
+      @RequestHeader("Authorization") String auth,
+      @RequestParam("ip") String ip,
+      @RequestParam("oid") String oid) throws IOException {
+
+    if (ip == null || oid == null) {
+      throw new IllegalArgumentException("[ERROR] Endpoint received null argument.");
     }
 
-    @GetMapping(value = "/rsuHeartbeat", produces = "application/json")
-    @ResponseBody
-    public static String heartBeat(@RequestHeader("Authorization") String auth, @RequestParam("ip") String ip, @RequestParam("oid") String oid) throws IOException {
+    TransportMapping transport = new DefaultUdpTransportMapping();
+    try (Snmp snmp = new Snmp(transport)) {
+      USM usm = new USM(
+          SecurityProtocols.getInstance(),
+          new OctetString(MPv3.createLocalEngineID()),
+          0);
 
-        if (ip == null || oid == null) {
-            throw new IllegalArgumentException("[ERROR] Endpoint received null argument.");
+      String username = null;
+      if (auth != null) {
+        String[] auth2 = auth.split(" ");
+        if (auth2[0].equals("Basic")) {
+          String unpw = auth2[1];
+          byte[] unpw2 = CodecUtils.fromBase64(unpw);
+          String unpw3 = new String(unpw2);
+          String[] unpw4 = unpw3.split(":");
+          if (unpw4.length == 2) {
+            username = unpw4[0];
+            String password = unpw4[1];
+            OctetString un = new OctetString(username);
+            UsmUser usmUser = new UsmUser(
+                un,
+                AuthMD5.ID,
+                new OctetString(password),
+                null,
+                null);
+            usm.addUser(un, usmUser);
+          }
         }
+      }
 
-        TransportMapping transport = new DefaultUdpTransportMapping();
-        try (Snmp snmp = new Snmp(transport)) {
-            USM usm = new USM(SecurityProtocols.getInstance(), new OctetString(MPv3.createLocalEngineID()), 0);
+      SecurityModels.getInstance().addSecurityModel(usm);
+      transport.listen();
 
-            String username = null;
-            if (auth != null) {
-              String[] auth2 = auth.split(" ");
-              if (auth2[0].equals("Basic")) {
-                String unpw = auth2[1];
-                byte[] unpw2 = CodecUtils.fromBase64(unpw);
-                String unpw3 = new String(unpw2);
-                String[] unpw4 = unpw3.split(":");
-                if (unpw4.length == 2) {
-                  username = unpw4[0];
-                  String password = unpw4[1];
-                  OctetString un = new OctetString(username);
-                  UsmUser usmUser = new UsmUser(
-                      un,
-                      AuthMD5.ID,
-                      new OctetString(password),
-                      null,
-                      null
-                  );
-                  usm.addUser(un, usmUser);
-                }
-              }
-            }
-
-            SecurityModels.getInstance().addSecurityModel(usm);
-            transport.listen();
-
-            return RsuSnmp.sendSnmpV3Request(ip, oid, snmp, username);
-        }
+      return RsuSnmp.sendSnmpV3Request(ip, oid, snmp, username);
     }
+  }
 
 }
